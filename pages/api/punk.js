13fc13d9f1d95fe77561bb915a2faccfafe17deb
botvader <eiglesias914@gmail.com>
2021-09-11 13:32:49 +0530

    1. set trait rarity 2. set punk rarity

--- pages/api/punk.js
+++ pages/api/punk.js
@@ -18,26 +18,37 @@ const get_all_traits = () => {
   return all_traits;
 }
 
-const get_trait_rarity = (all_traits, traits) => {
-  // Calculate rarity for given trait
-  
-  return all_traits;
+const set_trait_rarity = (all_traits, attributes) => {
+  for (let i = 0; i < attributes.length; i++) {
+    let attribute = attributes[i];
+    attribute['trait_count'] = all_traits[attribute.value];
+  }
+}
+
+const set_punk_rarity = (all_traits, punk) => {
+  let sumoftraits = 10000; //assuming
+  let { attributes } = punk;
+  let rarity_score = 0;
+  for (let i = 0; i < attributes.length; i++) {
+    let attribute = attributes[i];
+    attribute['rarity_score'] = 1 / (attribute['trait_count'] / sumoftraits);
+    rarity_score += attribute['rarity_score'];
+  }
+  punk['rarity_score'] = rarity_score;
 }
 
 const getPunk = (id) => {
   // Retrieve punk for id
-
   // Precompute the frequency of each trait
   let all_traits = get_all_traits();
   let punk = punks[id];
   let { attributes } = punk;
-
-  let metadata = get_trait_rarity(all_traits, attributes);
-  return { ...punk, metadata };
+  set_trait_rarity(all_traits, attributes);
+  set_punk_rarity(all_traits, punk);
+  return { ...punk };
 }
 
 export default function punkAPI(req, res) {
-
   const { id } = req.query
   const punk = getPunk(id)
   res.status(200).json({ punk })
