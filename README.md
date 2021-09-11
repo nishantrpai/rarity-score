0f62c40702911ceebf11a4bae28ab2d9809a4578
botvader <eiglesias914@gmail.com>
2021-09-11 12:52:12 +0530

    added punk rarity and attributes for getting started

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ References:
 - https://raritytools.medium.com/ranking-rarity-understanding-rarity-calculation-methods-86ceaeb9b98c
 - https://github.com/punkscape/01-rarity-analyser-hackathon
 
-Input: NFT
+Input: Punk id
 
 System: 
 - Get traits of NFT
 - Get rarity score for each trait
 - Return sum of all traits
 
-Output: Rarity score of NFT
+Output: Punk details + Rarity score
--- pages/api/punk.js
+++ pages/api/punk.js
@@ -1,6 +1,44 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import punks from '../../data/collection.json';
 
-export default function helloAPI(req, res) {
-    const { id } = req.query
-    res.status(200).json({ id })
+const get_all_traits = () => {
+  let all_traits = {};
+  for (let i = 0; i < punks.length; i++) {
+    let punk = punks[i];
+    let { attributes } = punk;
+    for (let j = 0; j < attributes.length; j++) {
+      let attribute = attributes[j]['value']
+      if (attribute in all_traits) {
+        all_traits[attribute]++;
+      } else {
+        all_traits[attribute] = 1;
+      }
+    }
+  }
+  return all_traits;
+}
+
+const get_trait_rarity = (all_traits, traits) => {
+  // Calculate rarity for given trait
+  
+  return all_traits;
+}
+
+const getPunk = (id) => {
+  // Retrieve punk for id
+
+  // Precompute the frequency of each trait
+  let all_traits = get_all_traits();
+  let punk = punks[id];
+  let { attributes } = punk;
+
+  let metadata = get_trait_rarity(all_traits, attributes);
+  return { ...punk, metadata };
+}
+
+export default function punkAPI(req, res) {
+
+  const { id } = req.query
+  const punk = getPunk(id)
+  res.status(200).json({ punk })
 }
