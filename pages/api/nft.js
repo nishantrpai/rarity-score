c5f92fdb30c9505690b3dda0382fe5970e192347
botvader <eiglesias914@gmail.com>
2021-09-11 17:30:53 +0530

    punk -> nft

--- pages/api/nft.js
+++ pages/api/nft.js
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import punks from '../../data/collection.json';
+import nfts from '../../data/collection.json';
 
 const get_all_traits = () => {
   let all_traits = {};
-  for (let i = 0; i < punks.length; i++) {
-    let punk = punks[i];
-    let { attributes } = punk;
+  for (let i = 0; i < nfts.length; i++) {
+    let nft = nfts[i];
+    let { attributes } = nft;
     for (let j = 0; j < attributes.length; j++) {
       let attribute = attributes[j];
       let { trait_type, value } = attribute;
@@ -32,18 +32,18 @@ const get_trait_rarity_score = (trait_type, all_traits) => {
   return all_traits[trait_type].sum;
 }
 
-const set_missing_traits = (punk, missing_traits, all_traits) => {
+const set_missing_traits = (nft, missing_traits, all_traits) => {
   // calculate rarity score for missing traits
-  punk['missing_traits'] = {};
+  nft['missing_traits'] = {};
   for (let i = 0; i < missing_traits.length; i++) {
     let missing_trait = missing_traits[i];
     let rarity_score = get_trait_rarity_score(missing_trait, all_traits);
-    punk['missing_traits'][missing_trait] = rarity_score
+    nft['missing_traits'][missing_trait] = rarity_score
   }
 }
 
-const set_trait_rarity = (punk, all_traits) => {
-  let { attributes } = punk;
+const set_trait_rarity = (nft, all_traits) => {
+  let { attributes } = nft;
   let missing_traits = Object.keys(all_traits);
   for (let i = 0; i < attributes.length; i++) {
     let attribute = attributes[i];
@@ -54,33 +54,33 @@ const set_trait_rarity = (punk, all_traits) => {
       missing_traits = missing_traits.filter(trait => trait !== trait_type);
     }
   }
-  set_missing_traits(punk, missing_traits, all_traits);
+  set_missing_traits(nft, missing_traits, all_traits);
 }
 
-const set_punk_rarity = (punk, all_traits) => {
+const set_nft_rarity = (nft, all_traits) => {
   let sumoftraits = all_traits['type'].sum; //All types humans, aliens 
-  let { attributes } = punk;
+  let { attributes } = nft;
   let rarity_score = 0;
   for (let i = 0; i < attributes.length; i++) {
     let attribute = attributes[i];
     attribute['rarity_score'] = 1 / (attribute['trait_count'] / sumoftraits);
     rarity_score += attribute['rarity_score'];
   }
-  punk['rarity_score'] = rarity_score;
+  nft['rarity_score'] = rarity_score;
 }
 
 const getPunk = (id) => {
-  // Retrieve punk for id
+  // Retrieve nft for id
   // Precompute the frequency of each trait
   let all_traits = get_all_traits();
-  let punk = punks[id];
-  set_trait_rarity(punk, all_traits);
-  set_punk_rarity(punk, all_traits);
-  return { ...punk };
+  let nft = nfts[id];
+  set_trait_rarity(nft, all_traits);
+  set_nft_rarity(nft, all_traits);
+  return { ...nft };
 }
 
-export default function punkAPI(req, res) {
+export default function nftAPI(req, res) {
   const { id } = req.query
-  const punk = getPunk(id)
-  res.status(200).json({ punk })
+  const nft = getPunk(id)
+  res.status(200).json({ nft })
 }
