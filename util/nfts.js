fefabde5506a6ecd35fedba347013bde22f13b62
botvader <eiglesias914@gmail.com>
2021-09-14 19:44:43 +0530

    calculate rarity scores once values are set

--- util/nfts.js
+++ util/nfts.js
@@ -46,10 +46,9 @@ const set_missing_traits = (nft, missing_traits, all_traits) => {
   for (let i = 0; i < missing_traits.length; i++) {
     let missing_trait = missing_traits[i];
     let rarity_count = get_trait_rarity_score(missing_trait, all_traits);
-    let missing_count = totaltraits - rarity_count; 
+    let missing_count = totaltraits - rarity_count;
     let rarity_score = (1 / (missing_count / totaltraits));
     nft['missing_traits'].push({ trait_type: missing_trait, rarity_score, missing_count });
-    nft['rarity_score'] += rarity_score;
   }
 }
 
@@ -61,9 +60,11 @@ const set_trait_rarity = (nft, all_traits) => {
       let attribute = attributes[i];
       if (attribute) {
         let { trait_type, value } = attribute;
-        attribute['trait_count'] = all_traits[trait_type][value];
-        // remove traits that are present
-        missing_traits = missing_traits.filter(trait => trait !== trait_type);
+        if (trait_type && value) {
+          attribute['trait_count'] = all_traits[trait_type][value];
+          // remove traits that are present
+          missing_traits = missing_traits.filter(trait => trait !== trait_type);
+        }
       }
     }
     set_missing_traits(nft, missing_traits, all_traits);
@@ -74,13 +75,22 @@ const set_nft_rarity = (nft, all_traits) => {
   let sumoftraits = all_traits['type'].sum; //All types humans, aliens 
   if (nft) {
     let { attributes } = nft;
-    let rarity_score = 0;
     for (let i = 0; i < attributes.length; i++) {
       let attribute = attributes[i];
       attribute['rarity_score'] = 1 / (attribute['trait_count'] / sumoftraits);
-      rarity_score += attribute['rarity_score'];
     }
-    nft['rarity_score'] = rarity_score;
+  }
+}
+
+const calculate_nft_rarity = (nft) => {
+  const { attributes, missing_traits } = nft;
+  nft['rarity_score'] = 0;
+  for (let i = 0; i < attributes.length; i++) {
+    nft['rarity_score'] += attributes[i]['rarity_score'];
+  }
+
+  for (let i = 0; i < missing_traits.length; i++) {
+    nft['rarity_score'] += missing_traits[i]['rarity_score'];
   }
 }
 
@@ -90,6 +100,7 @@ export const getNFT = (id) => {
   let nft = nfts[id];
   set_trait_rarity(nft, all_traits);
   set_nft_rarity(nft, all_traits);
+  calculate_nft_rarity(nft);
   return { ...nft };
 }
 
