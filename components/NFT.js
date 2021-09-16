c37543bf783d91aa183269e18f6fa021e60f6153
botvader <eiglesias914@gmail.com>
2021-09-16 06:55:41 +0530

    experiment with the styling

--- components/NFT.js
+++ components/NFT.js
@@ -11,16 +11,16 @@ export const NFT = (nft) => {
       >
         <img
           src={`https://ipfs.io/ipfs/${ipfs2http(nft.image)}`}
-          className="rounded-t-md px-4 bg-black" />
-        <div className="px-3 py-3 rounded-b-md text-red-500 
+          className="rounded-md px-4 bg-black" />
+        {/* <div className="px-3 py-3 rounded-b-md text-red-500 
         bg-red-100">
-          {/* <span className="flex text-xs mb-4 text-gray-500">#{nft.id}</span> */}
+           <span className="flex text-xs mb-4 text-gray-500">#{nft.id}</span> 
           <span className="flex max-w-max p-1 mb-1 text-xs 
           rounded rounded-sm text-red-500">
             {nft.rarity_score.toFixed(2)}
           </span>
           <h3 className="text-xs font-bold">{nft.name}</h3>
-        </div>
+  </div> */}
       </a>
     </>
   );
