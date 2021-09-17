0a8a7015f37e69981364c98ea6b3b28e31786c89
botvader <eiglesias914@gmail.com>
2021-09-17 07:23:09 +0530

    change grid design

--- components/NFT.js
+++ components/NFT.js
@@ -11,16 +11,10 @@ export const NFT = (nft) => {
       >
         <img
           src={`https://ipfs.io/ipfs/${ipfs2http(nft.image)}`}
-          className="rounded-md px-4 bg-black"  />
-        {/* <div className="px-3 py-3 rounded-b-md text-red-500 
-        bg-red-100">
-           <span className="flex text-xs mb-4 text-gray-500">#{nft.id}</span> 
-          <span className="flex max-w-max p-1 mb-1 text-xs 
-          rounded rounded-sm text-red-500">
-            {nft.rarity_score.toFixed(2)}
-          </span>
+          className="rounded-md px-4 bg-black" />
+        <div className="rounded-b-md mt-2">
           <h3 className="text-xs font-bold">{nft.name}</h3>
-  </div> */}
+        </div>
       </a>
     </>
   );
