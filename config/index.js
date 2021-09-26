ef7dab739bdf115d7f0120476a039fe98196b1f7
botvader <eiglesias914@gmail.com>
2021-09-26 11:47:51 +0530

    updated the config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 export const config = {
-    "API_URL": "punkscape-rarity.vercel.app",
-    "COLLECTION_NAME": "OneDayPunks",
-    "COLLECTION_TITLE": "One Day Punks",
-    "COLLECTION_DESCRIPTION": "10k \"One Day I'll Be A Punk\"-punks.",
-    "COLLECTION_IMG_LINK": "https://punkscape.xyz/og.png"
-}
+  API_URL: "odp-rarity.vercel.app",
+  COLLECTION_NAME: "OneDayPunks",
+  COLLECTION_TITLE: "One Day Punks",
+  COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
+  COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
+};
