dcf581bed08c85564d327e84f2ea35307ee7676b
nishantrpai <nishantrpai@gmail.com>
2022-05-23 19:58:47 +0530

    add dropdown for sidebar

--- components/AttrFilter.js
+++ components/AttrFilter.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
+import { RiArrowDropDownFill, RiArrowDropRightFill } from "react-icons/ri";
 import { json2query } from "../util";
 
 export const AttrFilter = (props) => {
+  const [showFilters, setShowFilters] = useState({});
   const router = useRouter();
   let { attr_count = "" } = router.query;
   const { attrCount } = props;
@@ -17,19 +19,33 @@ export const AttrFilter = (props) => {
     <>
       {filters.length > 1 && (
         <div className="text-xs mt-4" onChange={handleChange}>
-          <h3 className="font-bold px-2 text-gray-700 uppercase mb-2">
+          <h3
+            className="font-bold px-2 text-gray-700 flex items-center uppercase mb-2"
+            onClick={() => {
+              setShowFilters(!showFilters);
+            }}
+          >
+            <span className="text-xl">
+              {!showFilters ? (
+                <RiArrowDropRightFill />
+              ) : (
+                <RiArrowDropDownFill />
+              )}
+            </span>
             Attributes
           </h3>
-          <div className="px-2">
-            {filters.map((filter) => (
-              <a
-                className={`bg-white hover:bg-gray-300 rounded-md cursor-pointer text-gray-800 py-2 px-1 w-full flex`}
-                onClick={() => handleChange(filter)}
-              >
-                {filter} ({attrCount[filter]})
-              </a>
-            ))}
-          </div>
+          {showFilters && (
+            <div className="px-2">
+              {filters.map((filter) => (
+                <a
+                  className={`bg-white hover:bg-gray-300 rounded-md cursor-pointer text-gray-800 py-2 px-1 ml-2 w-full flex`}
+                  onClick={() => handleChange(filter)}
+                >
+                  {filter} ({attrCount[filter]})
+                </a>
+              ))}
+            </div>
+          )}
         </div>
       )}
     </>
--- components/Filters.js
+++ components/Filters.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { json2query } from "../util";
+import { RiArrowDropDownFill, RiArrowDropRightFill } from "react-icons/ri";
+import { useState } from "react";
 
 export const Filters = (props) => {
+  const [showFilters, setShowFilters] = useState({});
   const router = useRouter();
   let { traits = "" } = router.query;
   const { allTraits } = props;
@@ -24,24 +27,39 @@ export const Filters = (props) => {
             {/* if there is one common attribute, no need to filter based on that */}
             {Object.keys(allTraits[filter]["attributes"]).length > 1 && (
               <div className="w-full flex flex-col px-2 mt-4">
-                <h2 className="text-gray-700 uppercase font-bold mb-2">
+                <h2
+                  className="text-gray-700 flex uppercase items-center text-xs font-bold mb-2 cursor-pointer"
+                  onClick={() => {
+                    let tmpShowFilter = { ...showFilters };
+                    tmpShowFilter[filter] = !tmpShowFilter[filter];
+                    setShowFilters(tmpShowFilter);
+                  }}
+                >
+                  <span className="text-xl">
+                    {showFilters[filter] ? (
+                      <RiArrowDropRightFill />
+                    ) : (
+                      <RiArrowDropDownFill />
+                    )}
+                  </span>{" "}
                   {filter}
                 </h2>
-                {Object.keys(allTraits[filter]["attributes"])
-                  .sort((filter1, filter2) => {
-                    return (
-                      allTraits[filter]["attributes"][filter1].count -
-                      allTraits[filter]["attributes"][filter2].count
-                    );
-                  })
-                  .map((val) => (
-                    <a
-                      className={`bg-white cursor-pointer hover:bg-gray-300 hover:text-gray-900 rounded-md text-gray-700 py-2 px-1 flex`}
-                      onClick={() => handleChange(filter, val)}
-                    >
-                      {val} ({allTraits[filter]["attributes"][val].count})
-                    </a>
-                  ))}
+                {!showFilters[filter] &&
+                  Object.keys(allTraits[filter]["attributes"])
+                    .sort((filter1, filter2) => {
+                      return (
+                        allTraits[filter]["attributes"][filter1].count -
+                        allTraits[filter]["attributes"][filter2].count
+                      );
+                    })
+                    .map((val) => (
+                      <a
+                        className={`bg-white cursor-pointer hover:bg-gray-300 hover:text-gray-900 rounded-md text-gray-700 py-2 px-2 ml-2 flex`}
+                        onClick={() => handleChange(filter, val)}
+                      >
+                        {val} ({allTraits[filter]["attributes"][val].count})
+                      </a>
+                    ))}
               </div>
             )}
           </>
