c69ffcb8b6d024f2aac673b916c38446f02a44fa
nishantrpai <nishantrpai@gmail.com>
2022-06-20 13:21:31 +0530

    reset data

--- data/filterscript.js
+++ data/filterscript.js
@@ -1,13 +1,11 @@
 const collection = require("./collection.json");
-const fs = require("fs");
 /**
  * script for running any filters on collection
  */
 
-collection.map((val, i) => (collection[i]["id"] = i + 1));
-
-console.log(collection[0]);
-
-fs.writeFile("collection.json", JSON.stringify(collection), () => {
-  console.log("done");
-});
+for (let i = 0; i < collection.length; i++) {
+  for (let j = 0; j < collection[i]["attributes"].length; j++) {
+    let attribute = collection[i]["attributes"][j];
+    console.log(attribute);
+  }
+}
