6e84ba11fde136c4e0326464e49b5e94555ca9f2
nishantrpai <nishantpai@Nishants-MacBook-Air.local>
2022-06-20 13:06:20 +0530

    fix: 🐛 config
    start with whatever id you want

--- config/index.js
+++ config/index.js
@@ -6,7 +6,6 @@ export const config = {
   COLLECTION_TITLE: "One Day Punks",
   COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
   COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
-  STARTING_INDEX: 0,
   CONTRACT: {
     // if you're not sure, set CONTRACT: {}, will remove opensea link
     CHAIN: "ethereum", //possible values of ethereum, matic (polygin), klatyn, solana
--- data/filterscript.js
+++ data/filterscript.js
@@ -1,11 +1,13 @@
 const collection = require("./collection.json");
+const fs = require("fs");
 /**
  * script for running any filters on collection
  */
 
-for (let i = 0; i < collection.length; i++) {
-  for (let j = 0; j < collection[i]["attributes"].length; j++) {
-    let attribute = collection[i]["attributes"][j];
-    console.log(attribute);
-  }
-}
+collection.map((val, i) => (collection[i]["id"] = i + 1));
+
+console.log(collection[0]);
+
+fs.writeFile("collection.json", JSON.stringify(collection), () => {
+  console.log("done");
+});
