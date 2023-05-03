72a3633c95d3b88b5114633b6a02143b70fbff2d
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2023-05-03 20:01:26 +0530

    reduce weights to be normal

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-  env: "prod",
+  env: "local",
   LOCAL_API_URL: "localhost:3000",
   API_URL: "gnome-rarity-score.vercel.app",
   COLLECTION_NAME: "Sui Gnomes",
@@ -12,10 +12,10 @@ export const config = {
   // ADDRESS: "0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91", //note: for solana these values change, so currently not supported
   // },
   WEIGHTS: {
-    "Crystal sparkle": 15000,
-    "King's cloak": 3000,
-    "King's crown": 3000,
-    Princess: 1000,
-    "Princess' tiara": 1000,
+    "Crystal sparkle": 15,
+    "King's cloak": 30,
+    "King's crown": 30,
+    Princess: 10,
+    "Princess' tiara": 10,
   },
 };
