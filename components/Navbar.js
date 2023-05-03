7a1cf1e809749ef8e0611cdf91f3b3955c898a2b
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2023-05-03 18:13:36 +0530

    redirect title to home

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { IoMdClose } from 'react-icons/io';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 function Navbar({ title, menu, showMenu, setShowMenu }) {
   return (
     <div className="bg-black w-full flex justify-between items-center p-4">
-      <h1 className="text-lg text-white font-medium">{title}</h1>
-      {menu &&
-        <div className="md:hidden text-md text-white" onClick={() => setShowMenu(!showMenu)}>
+      <a className="text-lg text-white font-medium" href="/">
+        {title}
+      </a>
+      {menu && (
+        <div
+          className="md:hidden text-md text-white"
+          onClick={() => setShowMenu(!showMenu)}
+        >
           {showMenu ? <IoMdClose /> : <GiHamburgerMenu />}
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
 export default Navbar;
